8caa82b91feb3ee357de5ca3a7489197fdf0b080
Sara Tajernia <61985583+sara-tajernia@users.noreply.github.com>
2022-09-03 13:27:15 +0430

    Delete lab3 directory

--- lab3/Music/src/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"MusicPlayer","l":"MusicPlayer()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"MusicPlayer","l":"startPlaying(Music)"},{"p":"<Unnamed>","c":"MusicPlayer","l":"stop()"}]
